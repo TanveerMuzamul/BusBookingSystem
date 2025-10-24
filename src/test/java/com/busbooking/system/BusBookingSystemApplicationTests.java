54cbf2911e0d55031835585a85b1cc468bd3534b
tmuza <tmuza@192.168.0.12>
2025-10-24 02:04:32 +0100

    First Commit..

---
+++ src/test/java/com/busbooking/system/BusBookingSystemApplicationTests.java
@@ -0,0 +1,19 @@
+package com.busbooking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test will verify that the application context loads successfully
+    }
+
+    @Test
+    void basicTest() {
+        // Basic test to verify JUnit is working
+        assert true : "Basic test should pass";
+    }
+}
